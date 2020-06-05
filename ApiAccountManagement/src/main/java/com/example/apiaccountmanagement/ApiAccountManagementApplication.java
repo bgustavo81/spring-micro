a46fc00df5c9826b05b66b309e20cdfa079c3dac
Gustavo Bautista <bgustavo81@gmail.com>
2020-06-05 12:12:44 -0400

    first commit

---
+++ ApiAccountManagement/src/main/java/com/example/apiaccountmanagement/ApiAccountManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiaccountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAccountManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAccountManagementApplication.class, args);
+    }
+
+}
---
+++ ApiAccountManagement/src/test/java/com/example/apiaccountmanagement/ApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiaccountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAccountManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
